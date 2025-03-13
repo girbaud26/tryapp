7455a09006a66fb7a91ad95719c8571d8c10f25b
Aaron Paul <125766358+girbaud26@users.noreply.github.com>
2025-03-13 14:33:09 +0800

    Delete New folder directory

--- New folder/app.js
+++
@@ -1,9 +0,0 @@
-let score = 0;
-
-const button = document.getElementById('clickButton');
-const scoreDisplay = document.getElementById('score');
-
-button.addEventListener('click', () => {
-    score++;
-    scoreDisplay.textContent = score;
-});

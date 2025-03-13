6b5dbe7a2844b9a1e65c9af574cd64b285a29f6d
Aaron Paul <125766358+girbaud26@users.noreply.github.com>
2025-03-13 14:26:52 +0800

    Add files via upload

---
+++ New folder/app.js
@@ -0,0 +1,9 @@
+let score = 0;
+
+const button = document.getElementById('clickButton');
+const scoreDisplay = document.getElementById('score');
+
+button.addEventListener('click', () => {
+    score++;
+    scoreDisplay.textContent = score;
+});
